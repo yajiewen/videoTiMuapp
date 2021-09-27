906c4766edcbb56fd58fe704af980ec4c93f0d06
yajiewen <909092921@qq.com>
2021-09-28 00:35:39 +0800

    add titile detect

--- timu/urls.py
+++ timu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('addtimu/',add_timu),
     path('gettimu/<str:timuuuid>/',get_timu),
     path('deltimu/',del_timu),
+    path('detecttitle/',detect_timu),
 ]
--- timu/views.py
+++ timu/views.py
@@ -12,7 +12,7 @@ import pandas as ps
 api : http://127.0.0.1:8081/timu/addtimu/
 method: post
 数据: form-data
-    timufile: Subject.csv
+    Subject: Subject.csv
 
 返回内容:
 {
@@ -130,7 +130,7 @@ def get_timu(request,timuuuid):
 api : http://127.0.0.1:8081/timu/deltimu/
 method: post
 数据: form-data
-    timufile: Subject.csv
+    Subject: Subject.csv
 
 返回内容:
 response_data
@@ -193,3 +193,75 @@ def del_timu(request):
             return JsonResponse(response_data)
     else:
         return HttpResponse('Bad request',status = 500)
+
+
+"""
+------检测事故案例标题------
+api : http://127.0.0.1:8081/timu/detecttitle/
+method: post
+数据: form-data
+    Subject: Subject.csv
+    video: 
+    filename: video文件名
+
+返回内容:
+response_data
+"""
+def detect_timu(request):
+    if request.method == 'POST':
+        response_data={
+            'orderwrong':'no',
+            'haswrong':'no',
+            'wrongrow':[],
+        }
+
+        #获取题目文件
+        Subject = request.FILES.get('Subject')
+        # 保存题目文件文件
+        with open('Subject.xlsx','wb') as f:
+            for chunk in Subject.chunks():
+                f.write(chunk)
+        
+        #获取视频题目
+        Video = request.FILES.get('Video')
+        #保存视频题目
+        with open('video.xls','wb') as f:
+            for chunk in Video.chunks():
+                f.write(chunk)
+
+        #获取视频事故案例文件名
+        video_file_name = request.POST.get('filename')
+
+        #读取视频案例xls文件
+        rowList = []
+        df = ps.read_excel('video.xls',header=None)
+        rowList=df.values
+
+        # 开始获取视频文件生成uuid的文本
+        titles = []
+        for index in range(2,len(rowList)):
+            eachrow = rowList[index]
+            title = video_file_name.strip()+str(eachrow[0])+ eachrow[1].strip()
+            # print(title)
+            titles.append(title)
+
+        #读取题目文件
+        df1 = ps.read_excel('Subject.xlsx',header=None)
+        rowList2 = df1.values
+        row0 = rowList2[0]
+        print(row0)
+        timu_columlist = ['案例标题','Subject','rightAnswer1','wrongAnswer1','wrongAnswer2','wrongAnswer3']
+
+        if len(row0) == len(timu_columlist) and all(row0 == timu_columlist):
+            for index in range(1,len(rowList2)):
+                if rowList2[index][0].strip() not in titles:
+                    response_data['haswrong'] = 'yes'
+                    response_data['wrongrow'].append(index+1)
+        else:
+            response_data['orderwrong'] = 'yes'
+    
+        return JsonResponse(response_data)
+    else:
+        return HttpResponse("Bad requset",status=500)
+
+        
